e84e1399cce3c2c05daed877f16b5975281d7863
aplchiangh <alex.boquis@gmail.com>
2025-02-12 11:59:46 -0500

    Update URLs in Scout CLI formula for macOS and Linux releases

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -4,10 +4,10 @@ class Cli < Formula
   version "1.0.0"  # Change this to match your release
 
   if OS.mac?
-    url "https://github.com/scoutos/scout-cli/releases/download/latest/scout-cli-macos"
+    url "https://github.com/scoutos/cli/releases/download/latest/scout-cli-macos"
     sha256 "7dcffbdee67bd213c49978bebc7022939a87bfa1fa06a624667fff2a9025fdc9"
   elsif OS.linux?
-    url "https://github.com/scoutos/scout-cli/releases/download/v1.0.0/scout-cli-linux"
+    url "https://github.com/scoutos/cli/releases/download/v1.0.0/scout-cli-linux"
     sha256 "REPLACE_WITH_YOUR_LINUX_SHA256"
   end
 
