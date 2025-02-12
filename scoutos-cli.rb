2574bd35e0a4e484e1c9120e1cf181ea4c1b5491
Alex Boquist <alex.boquis@gmail.com>
2025-02-12 17:09:02 -0500

    Update scoutos-cli.rb

--- scoutos-cli.rb
+++ scoutos-cli.rb
@@ -5,10 +5,10 @@ class ScoutosCli < Formula  # <-- Class name must match filename "scoutos-cli.rb
 
   if OS.mac?
     url "https://github.com/scoutos/cli/releases/download/latest/scout-cli-macos"
-    sha256 "7dcffbdee67bd213c49978bebc7022939a87bfa1fa06a624667fff2a9025fdc9"
+    sha256 "9f9ee55810a95c37d6bf76f9a01e0a6c8cbc5a38b8ce6117326252f9830aa5d0"
   elsif OS.linux?
     url "https://github.com/scoutos/cli/releases/download/v1.0.0/scout-cli-linux"
-    sha256 "REPLACE_WITH_YOUR_LINUX_SHA256"
+    sha256 "8105740529fb1aa921aa3d49fd61ab0f4129783d992d1e2a346d7b062045f41c"
   end
 
   def install
